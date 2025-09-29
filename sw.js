fad81d304539ed840dbe2732ec4a1dcb4503bdc3
tpu-tndt <oam11@tpu.ru>
2025-09-29 22:18:34 +0700

    sw.js

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'robot-control-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json'
+];
+
+// Установка Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Кэширование файлов');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Активация Service Worker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Удаление старого кэша:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Перехват запросов
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Возвращаем кэшированную версию или делаем запрос
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
